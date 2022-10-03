1b15f47753b5d93156b7cd64d80557a1d9d964a5
alesiakaravai <lika.alesia@gmail.com>
2022-10-03 18:03:31 +0300

    add my_find_index and my_find_all

---
+++ my_enumerable.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+# all? any? include? none? each map! map size count
+# length select find reject max min
+
+module MyEnurmerable
+  def my_inject(value = nil)
+    result = value.nil? ? 0 : value
+    i = 0
+    while i < size
+      result = yield(result, self[i])
+      i += 1
+    end
+    result
+  end
+
+  def my_min
+    min_el = self[0]
+    i = 1
+    while i < size
+      min_el = self[i] if min_el > self[i]
+      i += 1
+    end
+    min_el
+  end
+
+  def my_find_index(value = nil)  #доделать с мэпами && give last elem
+    index
+    i = 0
+    if !value.nil?
+      while i < size
+        index = i if self[i] == value
+        i += 1
+      end
+    elsif block_given?
+      while i < size
+        index = i if yield(self[i])
+        i += 1
+      end
+    end
+    index
+  end
+
+  def my_find_all(value = nil)
+    index = []
+    i = 0
+    if !value.nil?
+      while i < size
+        index.push(self[i]) if self[i] == value
+        i += 1
+      end
+    elsif block_given?
+      while i < size
+        index.push(self[i]) if yield(self[i])
+        i += 1
+      end
+    end
+    index
+  end
+end
+
+class Array
+  include MyEnurmerable
+end
---
+++ task1.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative './my_enumerable'
+
+array1 = [100, 2, 3]
+array2 = [3, 4, 5]
+
+map1 = { foo: 0, bar: 1, baz: 0 }
+
+def f
+  yield(4)
+end
+
+def my_method
+  yield if block_given?
+  puts 'Привет из метода'
+end
+
+# f{|a| p "heelo world#{a}"}
+# p array2.inject(1000){|sum, value| sum + value}
+# p array2.my_inject{|sum, value| sum + value}
+
+p array1.my_find_all { |value| value > 0 }
