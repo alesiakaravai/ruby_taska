465c2e861205cf5f0c81a4862efdf7760976ddca
alesiakaravai <lika.alesia@gmail.com>
2022-10-17 22:27:31 +0300

    ME-1/fix_all

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,115 +3,134 @@
 module MyEnumerable
   def my_reject
     if block_given?
-      elements = []
-      each { |x| elements << x unless yield(x) }
-      elements
+      array = []
+      each { |element| array << element unless yield(element) }
+      array
     else
       to_enum(:my_reject)
     end
   end
 
-  def my_any?(value = nil)
-    if !value.nil?
-      each { |x| return true if value === x }
+  def my_any?(pattern = nil)
+    if !pattern.nil?
+      warn('given block not used', uplevel: 1) if block_given?
+      each { |element| return true if pattern === element }
     elsif block_given?
-      each { |x| return true if yield(x) }
+      each { |element| return true if yield(element) }
     else
-      each { |x| return true if x }
+      each { |element| return true if element }
     end
     false
   end
 
-  def my_all?(value = nil)
-    if !value.nil?
-      each { |x| return false unless value === x }
+  def my_all?(pattern = nil)
+    if !pattern.nil?
+      warn('given block not used', uplevel: 1) if block_given?
+      each { |element| return false unless pattern === element }
     elsif block_given?
-      each { |x| return false unless yield(x) }
+      each { |element| return false unless yield(element) }
     else
-      each { |x| return false unless x }
+      each { |element| return false unless element }
     end
     true
   end
 
-  def my_none?(value = nil)
-    if block_given?
-      each { |x| return false if yield(x) }
-    elsif !value.nil?
-      each { |x| return false if value === x }
+  def my_none?(pattern = nil)
+    if !pattern.nil?
+      warn('given block not used', uplevel: 1) if block_given?
+      each { |element| return false if pattern === element }
+    elsif block_given?
+      each { |element| return false if yield(element) }
     else
-      each { |x| return false if x }
+      each { |element| return false if element }
     end
     true
   end
 
-  def my_min(value = nil)
-     if value.nil?
-       min_value = self[0]
-       each { |i| min_value = i if min_value > i }
-       min_value
-     else
-       arr = self
-       (0...value).map {
-         arr.my_min
-         arr.delete(arr.my_min) }
-     end
-  end
-
-  def my_max
-    if value.nil?
-      max_value = self[0]
-      each { |i| max_value = i if max_value < i }
-      max_value
+  def my_min(element = nil, &block)
+    if element.nil?
+      min_element = self[0]
+      if block_given?
+        each { |i| min_element = i if yield(i, min_element).negative? }
+      else
+        each { |i| min_element = i if min_element > i }
+      end
+      min_element
     else
-      arr = self
-      (0...value).map {
-        arr.my_max
-        arr.delete(arr.my_max) }
+      array = self
+      return array if array.empty?
+
+      (0...element).map do
+        min_element = array.my_min(&block)
+        array.delete(min_element)
+      end
     end
   end
 
-  def my_find_index(value = nil)
-    if !value.nil?
-      (0..size).each { |x| return x if self[x] == value}
+  def my_max(element = nil, &block)
+    if element.nil?
+      max_element = self[0]
+      if block_given?
+        each { |i| max_element = i if yield(i, max_element).positive? }
+      else
+        each { |i| max_element = i if max_element < i }
+      end
+      max_element
+    else
+      array = self
+      return array if array.empty?
+
+      (0...element).map do
+        max_element = array.my_max(&block)
+        array.delete(max_element)
+      end
+    end
+  end
+
+  def my_find_index(object = nil)
+    if !object.nil?
+      warn('given block not used', uplevel: 1) if block_given?
+      (0..size).each { |x| return x if self[x] == object }
     elsif block_given?
-      (0..size).each { |x| return x if yield(self[x])}
-    else 
+      (0..size).each { |x| return x if yield(self[x]) }
+    else
       return to_enum(:my_find_index)
     end
     nil
   end
 
   def my_find(if_none_proc = nil)
-    to_enum(:my_find) if !block_given?
+    return to_enum(:my_find) if !block_given? && if_none_proc.nil?
+    return to_enum(:my_find, if_none_proc) if !block_given? && !if_none_proc.nil?
 
-    each {|x| return x if yield(x)}
+    each { |element| return element if yield(element) }
     if_none_proc.nil? ? nil : if_none_proc.call
   end
 
-  def my_find_all(value = nil)
-    elements = []
-    if !value.nil?
-      each {|x| elements << x if x == value}
-    elsif block_given?
-      each {|x| elements << x if yield(x)}
-    end
-    elements
+  def my_find_all
+    return to_enum(:my_find_all) unless block_given?
+
+    array = []
+    each { |x| array << x if yield(x) }
+    array
   end
 
   def my_select
-    to_enum(:my_select) if !block_given?
-    elements = []
-    each { |x| elements << x if yield(x) }
-    elements
+    return to_enum(:my_select) unless block_given?
+
+    array = []
+    each { |x| array << x if yield(x) }
+    array
   end
 
-  def my_count(value = nil)
+  def my_count(object = nil)
     count = 0
-    
-    if !value.nil?
-      each {|x| count += 1 if value == x}
+
+    if !object.nil?
+      warn('given block not used', uplevel: 1) if block_given?
+      each { |element| count += 1 if object == element }
     elsif block_given?
-      each {|x| count += 1 if yield(x)}
+      each { |element| count += 1 if yield(element) }
     else
       count = length
     end
@@ -119,16 +138,15 @@ module MyEnumerable
   end
 
   def my_map
-    if block_given?
-      elements = []
-      each {|x| elements << yield(x)}
-      return elements
-    end
-    self
+    return to_enum(:my_map) unless block_given?
+
+    array = []
+    each { |x| array << yield(x) }
+    array
   end
 
-  def my_include?(value)
-    each {|x| return true if value == x}
+  def my_include?(object)
+    each { |element| return true if object == element }
     false
   end
 end
--- task1.rb
+++ task1.rb
@@ -5,15 +5,6 @@ require_relative './my_enumerable'
 array1 = [100, 5, 3]
 array2 = [3, 4, 5]
 
-def f
-  yield(4)
-end
-
-def my_method
-  yield if block_given?
-  puts 'Привет из метода'
-end
-
 # f{|a| p "heelo world#{a}"}
 # p array2.inject(1000){|sum, value| sum + value}
 # p array2.my_inject{|sum, value| sum + value}
@@ -22,27 +13,25 @@ end
 
 # p array1.my_any?(5)
 
-#enum = Enumerator.new { |y| array1.each { |x| y << x } }
+# enum = Enumerator.new { |y| array1.each { |x| y << x } }
 
 # p enum
 
 # p array1.my_any?(Integer)
 
-#p (1..4).my_all?
+# p (1..4).my_all?
 # p enum
 
-#p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-#p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-#p %w{ant bear cat}.my_none?(/d/)                        #=> true
-#p [1, 3.14, 42].my_none?(Float)                         #=> false
-#p [].my_none?                                           #=> true
-#p [nil].my_none?                                        #=> true
-#p [nil, false].my_none?                                 #=> true
-#p [nil, false, true].my_none?                           #=> false
+# p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# p %w{ant bear cat}.my_none?(/d/)                        #=> true
+# p [1, 3.14, 42].my_none?(Float)                         #=> false
+# p [].my_none?                                           #=> true
+# p [nil].my_none?                                        #=> true
+# p [nil, false].my_none?                                 #=> true
+# p [nil, false, true].my_none?                           #=> false
 
-#p array1.min(2){|a, b| a == b}
+# p array1.min(2){|a, b| a == b}
 
-p array1.count(100){|x| x % 1 == 0}
-
-
-
+# p array1.my_select{|x| x > 5}
+p [].find_all { |x| x > 5 }
